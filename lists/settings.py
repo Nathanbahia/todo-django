7d128f5039a75b2f6200d68be27daffe1768325c
nathanbahia <nathanbabahia@gmail.com>
2020-08-25 10:58:00 -0300

    Criação de sistema de login close #3

--- lists/settings.py
+++ lists/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+    'users',
     'bootstrap4',
 ]
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'lists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +117,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
